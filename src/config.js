097400499edcc5d98322d293b4aa9ae3ddccebf0
Oliver Scott <olivercscott@gmail.com>
2023-07-20 10:04:19 -0400

    calyxos: Add CalyxOS specific configuration
    * Change name and description to CalyxOS

--- src/config.js
+++ src/config.js
@@ -1,32 +1,6 @@
 export default {
     OS_NAME: import.meta.env.VITE_APP_OS_NAME,
     ACCENT_COLOR: "#0366d6",
-    SUPPORTED_DEVICES: [
-        {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
-        },
-    ],
     RELEASE_VARIANTS: {
         minimal: {
             description: "Minimal version without Google apps or services.",
@@ -46,22 +20,4 @@ export default {
             suffix: "beta",
         },
     },
-    DONATION_LINKS: [
-        {
-            title: "Recurring donation",
-            description:
-                "Please consider making a recurring donation on Patreon for benefits such as early access to updates, exclusive behind-the-scenes development news, and priority support. This allows us to support the project sustainably in the future.",
-            highlight: true,
-            url: "https://patreon.com/kdrag0n",
-            icon: "patreon",
-        },
-        {
-            title: "One-time donation",
-            description:
-                "You can also make a one-time donation through PayPal. It doesn’t help with sustainable support, but every little bit is appreciated!",
-            highlight: false,
-            url: "https://paypal.me/kdrag0ndonate",
-            icon: "paypal",
-        },
-    ],
 };
